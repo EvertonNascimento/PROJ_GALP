2f0eab2075adaac05b6dfb929409ed0dedc2a933
EvertonNascimento <e.nascimento@campus.fct.unl.pt>
2022-06-27 17:11:11 +0100

    Update main.tf

--- proj/main.tf
+++ proj/main.tf
@@ -3,12 +3,13 @@ provider "aws" {
  
 }
 
-module network {
-    source = "../modules/terraform_aws_network"
-    network_cidr = "172.168.0.0/16"
-    n_subnets = "3"
-    infra_name = "test_sre"
-    tags = { infra = "test"}
+
+module "network" {
+  source       = "../modules/terraform_aws_network"
+  network_cidr = var.network_cidr
+  n_subnets    = var.n_subnets
+  infra_name   = var.infra_name
+  tags         = { infra = "test" }
 
 }
 
@@ -24,23 +25,22 @@ module network {
 */
 
 module "instances" {
-    source = "../modules/terraform_aws_instances"
-    ami           = "ami-09e513e9eacab10c1" # ubuntu 22.04 eu-west
-    instance_type = "t2.micro"
-    #n_subnets = ceil((var.n_subnets)/2)
-    n_subnets = "3" #3 is the max number of subnets
-    #networks = module.network.networks
-    bastion_subnet = module.network.network_info.public_subnets[0]
-    private_subnets = module.network.network_info.private_subnets
-    public_subnets = module.network.network_info.public_subnets
-    bastion_nic = module.network.network_info.bastion_nic
-    private_nics = module.network.network_info.private_nics
-    public_security_group = module.network.network_info.public_security_group
-    private_security_group = module.network.network_info.private_security_group
-    bastion_security_group = module.network.network_info.bastion_security_group
-    loadbalancer_sg = module.network.network_info.loadbalancer_sg
-    target_group_arn = module.network.network_info.target_group_arn
-    
+  source        = "../modules/terraform_aws_instances"
+  ami           = var.ami # ubuntu 22.04 eu-west
+  instance_type = var.instance_type
+  #n_subnets = ceil((var.n_subnets)/2)
+  n_subnets = var.n_subnets #3 is the max number of subnets
+  bastion_subnet         = module.network.network_info.public_subnets[0]
+  private_subnets        = module.network.network_info.private_subnets
+  public_subnets         = module.network.network_info.public_subnets
+  bastion_nic            = module.network.network_info.bastion_nic
+  private_nics           = module.network.network_info.private_nics
+  public_security_group  = module.network.network_info.public_security_group
+  private_security_group = module.network.network_info.private_security_group
+  bastion_security_group = module.network.network_info.bastion_security_group
+  loadbalancer_sg        = module.network.network_info.loadbalancer_sg
+  target_group_arn       = module.network.network_info.target_group_arn
+
 }
 
 
