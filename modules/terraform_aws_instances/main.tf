547d45f2c9e7860f113e9aef92d1e6c493649611
EvertonNascimento <e.nascimento@campus.fct.unl.pt>
2022-06-27 15:58:56 +0100

    Update main.tf

--- modules/terraform_aws_instances/main.tf
+++ modules/terraform_aws_instances/main.tf
@@ -1,17 +1,13 @@
-#loop nos valores de nics e vms
-#meter usernames
-#
+
 resource "aws_key_pair" "deployer-key" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLug4pJFWqb+A2nTK5yEFd+IX8X+ziTngZWcZslhGRPM/kOKxBDAvWZS0qYAXe1RobN0IftZbehGp5ScPVXBy0Tq4/PmJUg6r4w2KjJlAYv6gPDzNvZjnZ7TOOchZNCHxttJHNt5FvLu53r4GGtu9hk9P4tATazW21qGR0YmGtHT7dD1mSIXsMqQoyXxicJ7S0cU0vVX0a1XsYANwZ+Y1O0XfMe25vn8JF2gHY2ZMD7qGAAWheBheTpwCHwz9Te3I38DL7NH1mqCvAC9QIUSJaslNWfuEqMp4oFsdl6GDimVK3AK/bCaooYJq5PyyaZPU3i0PenS6ZEFvKw5csFJCR6kHyMSGKUB3p0Ab4RXCtDpl37PCcnirIn1KaR/zvl0yBNDRz0lFLHzkNNXA8/dsDK9aUI9urcAKvJoTWE09C7iPFOwjd7Ap3u5FGB3MjHcZHZsQ+VRCIVluBfze/dnHYpge7/0x/ktWCzwRHxyJrhyy3vuTW+NIJq7ss7NhmhNM= timestamp/everton.nascimento@TSSILAP0465"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDG3BCPh2N+4GYqQVfv4zT8m/JzRNxOhnHAfXaphMuH0y9+Q1TDfsYnna/UqZSy2c5Df5wONS8oNgkESDCK9s+qZEJAf+y8pMZ2uZ05BrKhKtzaKGGFh8WQ8M+cJpVlWQy9J3Joiyo7VO0eDnj+Gr16FkHM1l5B9BQ3s/HZwHdtHFG4/TsrmwS3cUq1jgPgYKVqPKx0jeSToQCOxQnID/EZ0ruoa3jzlG7COjU7hpP8sQiSlfrw+WQq4su+p8gAlE0glrrM7u/tPbAE8TER31OLp51MBHWN96zxyG9VyhhHVV00lk2YsT4c6dThBMYWaqaUgRR7shWDPsRjCa2OPq/8Xo3sjUpu5dL2nxsOBHCcTesBtPhAL/Mg+7WbzKI73pwoprfJxV49RLnzpVFQC1Te4lXJXBI/4PrCOehfHYf7lKmGCMgbku9Z7nQO69tu8RUsNHQbNfI+8sNmIvAIHN3HtK55hqDsAvbDRXCA4WzdUU7QxKdbJwsqRfEc1iQdyhs="
 }
 
 
 resource "aws_instance" "bastion_instance" {
   ami           = var.ami # ubuntu 22.04
   instance_type = var.instance_type
-  #security_groups = aws_security_group.public_sg.id
-  #security_groups = [var.bastion_security_group]
   key_name         = "deployer-key"
   network_interface {
     network_interface_id = var.bastion_nic
@@ -27,26 +23,46 @@ resource "aws_eip" "bastion-ip" {
 }
 
 
-
 resource "aws_instance" "testvms" {
   count = var.n_subnets
   ami           = var.ami # ubuntu 22.04
   instance_type = var.instance_type
-  #security_groups = aws_security_group.private_sg.id
-  #security_groups = [var.private_security_group]
+  key_name         = "deployer-key"
 
   network_interface {
-    #network_interface_id = aws_network_interface.test_nic1.id
     network_interface_id = var.private_nics[count.index]
     device_index         = 0
     }
+user_data = <<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y apache2
+sudo chown -R ubuntu:ubuntu /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<\!DOCTYPE html><html><body><p>Hello World at "$(date +"%Y-%m-%d")"</p></body></html>" > /var/www/html/index.html
+EOF
 }
 
-/*
-#instances are associated with target group
-resource "aws_lb_target_group_attachment" "private_subs_attachment" {
-  target_group_arn = aws_lb_target_group.private_target_group.id
-  target_id        = aws_instance.testvms.*.id
+
+resource "aws_network_interface_sg_attachment" "sg_bastion_attachment" {
+  security_group_id    = var.bastion_security_group
+  network_interface_id = var.bastion_nic
+}
+
+
+resource "aws_network_interface_sg_attachment" "priv_sub_attachment" {
+  count = var.n_subnets
+  security_group_id    = var.private_security_group
+  network_interface_id = var.private_nics[count.index]
+}
+
+
+
+resource "aws_lb_target_group_attachment" "alb_attachment" {
+  count = var.n_subnets
+  target_group_arn = var.target_group_arn
+  target_id        = aws_instance.testvms[count.index].id
   port             = 80
 }
-*/
