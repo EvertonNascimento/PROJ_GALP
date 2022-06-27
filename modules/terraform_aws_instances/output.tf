f7d082b112f141fd3fecd13de119ae39bb905e7b
EvertonNascimento <e.nascimento@campus.fct.unl.pt>
2022-06-27 16:21:08 +0100

    Update output.tf

--- modules/terraform_aws_instances/output.tf
+++ modules/terraform_aws_instances/output.tf
@@ -1,16 +1,13 @@
-/*
-
 output "private_vms_ips" {
-  value       = aws_instance.testvms.*.ip
-  description = "VPC id, List of all public, private and db subnet IDs"
+  value       = [aws_instance.testvms.*.private_ip]
+  description = "private machines ips"
 }
 
 output "bastion_public_ip" {
-  value       = aws_instance.bastion_instance.public_ip
-  description = "VPC id, List of all public, private and db subnet IDs"
+  value       = [aws_instance.bastion_instance.*.public_ip]
+  description = "bastion machine ip"
 }
 
 output "ssh_key" {
-  
+  value = aws_key_pair.deployer-key.public_key
 }
-*/
