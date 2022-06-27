324b9f20994e4adf8470dc326124485531da8b28
EvertonNascimento <e.nascimento@campus.fct.unl.pt>
2022-06-27 17:22:21 +0100

    Update main.tf

--- modules/terraform_aws_instances/main.tf
+++ modules/terraform_aws_instances/main.tf
@@ -1,7 +1,6 @@
-
 resource "aws_key_pair" "deployer-key" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDG3BCPh2N+4GYqQVfv4zT8m/JzRNxOhnHAfXaphMuH0y9+Q1TDfsYnna/UqZSy2c5Df5wONS8oNgkESDCK9s+qZEJAf+y8pMZ2uZ05BrKhKtzaKGGFh8WQ8M+cJpVlWQy9J3Joiyo7VO0eDnj+Gr16FkHM1l5B9BQ3s/HZwHdtHFG4/TsrmwS3cUq1jgPgYKVqPKx0jeSToQCOxQnID/EZ0ruoa3jzlG7COjU7hpP8sQiSlfrw+WQq4su+p8gAlE0glrrM7u/tPbAE8TER31OLp51MBHWN96zxyG9VyhhHVV00lk2YsT4c6dThBMYWaqaUgRR7shWDPsRjCa2OPq/8Xo3sjUpu5dL2nxsOBHCcTesBtPhAL/Mg+7WbzKI73pwoprfJxV49RLnzpVFQC1Te4lXJXBI/4PrCOehfHYf7lKmGCMgbku9Z7nQO69tu8RUsNHQbNfI+8sNmIvAIHN3HtK55hqDsAvbDRXCA4WzdUU7QxKdbJwsqRfEc1iQdyhs="
+  public_key = var.public_key
 }
 
 
@@ -33,7 +32,7 @@ resource "aws_instance" "testvms" {
     network_interface_id = var.private_nics[count.index]
     device_index         = 0
     }
-user_data = <<EOF
+    user_data = <<EOF
 #!/bin/bash
 sudo apt update -y
 sudo apt install -y apache2
